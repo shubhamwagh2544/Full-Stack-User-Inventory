c1d4a696a4cbe97f8a98eb12ee15b878c6eb9241
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-05 09:23:54 +0530

    start

---
+++ src/main/java/com/dashboard/userinventory/UserInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.userinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashboard/userinventory/UserInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.userinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
