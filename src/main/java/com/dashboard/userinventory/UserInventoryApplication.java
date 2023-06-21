b1cbfd25b29de4943d704fbf4e852d0ae865a596
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-21 18:55:09 +0530

    CD test

--- src/main/java/com/dashboard/userinventory/UserInventoryApplication.java
+++ src/main/java/com/dashboard/userinventory/UserInventoryApplication.java
@@ -8,9 +8,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserInventoryApplication {
+
+	@GetMapping(path = "hello")
+	public String hello() {
+		return "Hello! World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserInventoryApplication.class, args);
