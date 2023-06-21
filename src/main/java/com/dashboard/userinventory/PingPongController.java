9b9ef13d2a0875ccc1824d6df36d3b7a513997c9
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-21 17:52:02 +0530

    CD Workflow

--- src/main/java/com/dashboard/userinventory/PingPongController.java
+++ src/main/java/com/dashboard/userinventory/PingPongController.java
@@ -1,6 +1,5 @@
 package com.dashboard.userinventory;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
