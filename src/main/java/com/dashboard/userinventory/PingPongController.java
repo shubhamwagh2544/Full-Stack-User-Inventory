c40ea48f365fb10b85bcf5898dd1ac73a5fe0f3d
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-22 11:37:48 +0530

    CD-Slack Integration

--- src/main/java/com/dashboard/userinventory/PingPongController.java
+++ src/main/java/com/dashboard/userinventory/PingPongController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
-    record PingPong(String pingpong) {}
+    record PingPong(String ping) {}
     @GetMapping("pingpong")
     public PingPong pingPong() {
         return new PingPong("ping-pong");
